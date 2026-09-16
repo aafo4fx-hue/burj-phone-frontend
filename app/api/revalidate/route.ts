000b54d71db562a9e5c09353ba64ae3edd98f1fa
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-09-16 07:44:25 +0300

    S

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -3,6 +3,6 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   const tag = req.nextUrl.searchParams.get("tag");
-  if (tag) revalidateTag(tag);
-  return NextResponse.json({ revalidated: true });
+  if (tag) revalidateTag(tag, "layout");
+  return NextResponse.json({ revalidated: true }, { status: 200 });
 }
